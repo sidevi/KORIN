bcca610ba546e6bc67e5ed52b665681db53711cb
okosun <okosunmicheal123@gmail.com>
2023-12-14 21:57:36 -0700

    my first line

---
+++ music.js
@@ -0,0 +1,9 @@
+const musicalNote = document.getElementById("musical-note");
+const title = document.getElementById("title");
+const progresscontainer = document.getElementById("progress-container");
+const musicinfo = document.getElementById("music-info");
+const progress = document.getElementById("progress");
+const audio = docuemnt.getElementById("audio");
+const cover = document.getElementById("cover");
+const prev = document.getElementById("prev");
+
